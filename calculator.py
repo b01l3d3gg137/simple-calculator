d590b72c2a662f5c09d9a1ffc26b12897afc70b2
b01l3d3gg137 <mzr.0cx@gmail.com>
2024-05-20 10:05:12 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# TUGAS PEMOGRAMAN PYTHON
+# by Veniel Garwita
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    print("Addition: 2 + 3 =", add(2, 3))
+    print("Subtraction: 5 - 2 =", subtract(5, 2))
+    print("Multiplication: 4 * 3 =", multiply(4, 3))
+    print("Division: 10 / 2 =", divide(10, 2))
